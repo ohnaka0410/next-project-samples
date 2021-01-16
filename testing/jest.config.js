8409319c7379ab0cd4fab4ba94a94e804ed0eed9
ohnaka0410 <>
2021-01-17 00:47:11 +0900

    update load ts-config

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -3,14 +3,13 @@
  */
 const { pathsToModuleNameMapper } = require("ts-jest/utils");
 const { readFileSync } = require("fs");
-const { parse } = require("json5");
-const {compilerOptions} = parse(readFileSync("./tsconfig.json"));
+const { parse } = require("jsonc-parser");
+const { compilerOptions } = parse(readFileSync("./tsconfig.json").toString());
 const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" });
 
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  roots: ["<rootDir>/src"],
   moduleNameMapper,
   globals: {
     "ts-jest": {
